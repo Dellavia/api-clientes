4140a4846b49cddd375248e3e7ac7a60f05bba33
Denis Tofoli <denis_tofoli@msn.com>
2023-11-09 10:21:19 -0300

    feat: DDD / Dt. Nasc

--- app/models/cliente.py
+++ app/models/cliente.py
@@ -6,6 +6,7 @@ class Cliente(BaseModel):
     id = db.Column(db.String(14), primary_key=True)
     nome = db.Column(db.String(100))
     email = db.Column(db.String(100))
+    ddd = db.Column(db.String(3))
     telefone = db.Column(db.String(25))
     endereco = db.Column(db.String(100))
     bairro = db.Column(db.String(100))
@@ -14,6 +15,7 @@ class Cliente(BaseModel):
     cep = db.Column(db.String(9))
     sexo = db.Column(db.String(1))
     ult_compra = db.Column(db.String(8))
+    dt_nascimento = db.Column(db.String(8))
 
 
     def __repr__(self):
