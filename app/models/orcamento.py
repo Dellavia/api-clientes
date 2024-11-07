be489c956627c939faaa5f896313144e5b16ce71
Denis Tofoli <denis_tofoli@msn.com>
2024-11-07 15:34:15 -0300

    feat: add coluna faturamento

--- app/models/orcamento.py
+++ app/models/orcamento.py
@@ -7,6 +7,8 @@ class Orcamento(BaseModel):
     emissao = db.Column(db.String(8))
     operador = db.Column(db.String(6))
     loja = db.Column(db.String(2))
+    dtemisnf = db.Column(db.String(8))
+    vlrbruto = db.Column(db.Float)
 
     def __repr__(self):
         return f'<Orcamento "{self.id}">'
